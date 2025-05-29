cb878c730ca785737f0bd3f791b669c2c9fc0152
Flipstienen <flipstienen1@gmail.com>
2025-05-29 19:22:43 +0200

    product page and sidebar.
    I added i productpage, this isn't how it will be in the end, but it is a start. I also changed our header to a sidebar that can be opened and closed.

--- KE03_INTDEV_SE_2_Base/wwwroot/js/site.js
+++ KE03_INTDEV_SE_2_Base/wwwroot/js/site.js
@@ -1,4 +1,19 @@
-﻿// Please see documentation at https://learn.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
+﻿function toggleSidebar() {
+    const navItems = document.getElementsByClassName("navigation");
+    document.getElementById("sidebar").classList.toggle("collapsed");
+    if (navItems[0].style.display === "none") {
+        //document.getElementById("closed").style.display = "none"
 
-// Write your JavaScript code.
+        for (let i = 0; i < navItems.length; i++) {
+            navItems[i].style.display = "block";
+        }
+    }
+
+    else {
+        //document.getElementById("closed").style.display = "block"
+
+        for (let i = 0; i < navItems.length; i++) {
+            navItems[i].style.display = "none"
+        }
+    }
+}
