f9f07c78faaf945ca65e02bef0140fc12deff9ea
Kevin Huijten <1844067huijten@zuyd.nl>
2025-05-30 02:10:27 +0200

    Changed styling on sidebar.

--- KE03_INTDEV_SE_2_Base/wwwroot/js/site.js
+++ KE03_INTDEV_SE_2_Base/wwwroot/js/site.js
@@ -1,19 +1,30 @@
 ﻿function toggleSidebar() {
+    const sidebar = document.getElementById("sidebar");
     const navItems = document.getElementsByClassName("navigation");
-    document.getElementById("sidebar").classList.toggle("collapsed");
-    if (navItems[0].style.display === "none") {
-        //document.getElementById("closed").style.display = "none"
+    const toggleBtn = document.getElementById("toggleBtn");
 
+    if (sidebar.style.width === "60px") {
+        // Expand sidebar
+        sidebar.style.width = "250px";
+        ProfileSection.style.display = "block";
         for (let i = 0; i < navItems.length; i++) {
             navItems[i].style.display = "block";
         }
-    }
-
-    else {
-        //document.getElementById("closed").style.display = "block"
-
+        toggleBtn.innerHTML = `
+            <span style="display:block; width: 22px; height: 2px; background-color: black; margin: 4px 0;"></span>
+            <span style="display:block; width: 22px; height: 2px; background-color: black; margin: 4px 0;"></span>
+            <span style="display:block; width: 22px; height: 2px; background-color: black; margin: 4px 0;"></span>
+        `;
+    } else {
+        // Collapse sidebar
+        sidebar.style.width = "60px";
         for (let i = 0; i < navItems.length; i++) {
-            navItems[i].style.display = "none"
+            navItems[i].style.display = "none";
         }
+        // Change toggle button icon to a ">" (right arrow)
+        toggleBtn.innerHTML = "&gt;";
+        toggleBtn.style.fontSize = "24px"; // make it bigger to look good
+        toggleBtn.style.lineHeight = "24px";
+        ProfileSection.style.display = "none";
     }
 }
