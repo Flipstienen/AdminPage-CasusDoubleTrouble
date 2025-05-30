c2bbd0aef4eca7903e30f5370124c94511257346
ThatDevFoxy <ThatDevFoxy@gmail.com>
2025-05-30 15:34:20 +0200

    Slight edits to the Navbar
    - Edited the user to be person instead of a placeholder for professionalism
    - Changed icons being small and offcentered
    - Moved the hamburger menu to the left
    - Slightly animated the change between collapsing and opening

--- KE03_INTDEV_SE_2_Base/wwwroot/js/site.js
+++ KE03_INTDEV_SE_2_Base/wwwroot/js/site.js
@@ -2,28 +2,57 @@
     const sidebar = document.getElementById("sidebar");
     const navItems = document.getElementsByClassName("navigation");
     const toggleBtn = document.getElementById("toggleBtn");
+    const ProfileSection = document.getElementById("ProfileSection");
+    const navImages = sidebar.querySelectorAll("ul.nav img");
 
-    if (sidebar.style.width === "60px") {
-        // Expand sidebar
+    const isCollapsed = sidebar.style.width === "60px";
+
+    if (isCollapsed) {
         sidebar.style.width = "250px";
         ProfileSection.style.display = "block";
+
         for (let i = 0; i < navItems.length; i++) {
             navItems[i].style.display = "block";
         }
+
+        for (let img of navImages) {
+            img.classList.add("me-2");
+            img.style.width = "20px";
+        }
+
+        sidebar.querySelectorAll('a.quickselectsidebar.nav-link').forEach(link => {
+            link.style.padding = '';
+            link.classList.remove("mt-2")
+        });
+
         toggleBtn.innerHTML = `
             <span style="display:block; width: 22px; height: 2px; background-color: black; margin: 4px 0;"></span>
             <span style="display:block; width: 22px; height: 2px; background-color: black; margin: 4px 0;"></span>
             <span style="display:block; width: 22px; height: 2px; background-color: black; margin: 4px 0;"></span>
         `;
+        toggleBtn.style.fontSize = "";
+        toggleBtn.style.lineHeight = "";
+
     } else {
-        // Collapse sidebar
         sidebar.style.width = "60px";
+        ProfileSection.style.display = "block";
+
         for (let i = 0; i < navItems.length; i++) {
             navItems[i].style.display = "none";
         }
-        // Change toggle button icon to a ">" (right arrow)
+
+        for (let img of navImages) {
+            img.classList.remove("me-2");
+            img.style.width = "30px";
+        }
+
+        sidebar.querySelectorAll('a.quickselectsidebar.nav-link').forEach(link => {
+            link.style.padding = '0';
+            link.classList.add("mt-2")
+        });
+
         toggleBtn.innerHTML = "&gt;";
-        toggleBtn.style.fontSize = "24px"; // make it bigger to look good
+        toggleBtn.style.fontSize = "24px";
         toggleBtn.style.lineHeight = "24px";
         ProfileSection.style.display = "none";
     }
