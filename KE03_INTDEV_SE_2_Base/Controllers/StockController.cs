02694f52dc2a61e7349311d06cb587d8b960dc1e
ThatDevFoxy <ThatDevFoxy@gmail.com>
2025-06-02 15:08:32 +0200

    Creation Stock page
    - Added stock page index
    - Changed routing from /Stocks to /Stock
    - Added stock controller

---
+++ KE03_INTDEV_SE_2_Base/Controllers/StockController.cs
@@ -0,0 +1,21 @@
+﻿using DataAccessLayer;
+using Microsoft.AspNetCore.Mvc;
+
+namespace KE03_INTDEV_SE_2_Base.Controllers
+{ 
+public class StockController : Controller
+{
+    private readonly MatrixIncDbContext _context;
+
+    public StockController(MatrixIncDbContext context)
+    {
+        _context = context;
+    }
+
+    public IActionResult Index()
+    {
+        var parts = _context.Parts.ToList();
+        return View(parts);
+    }
+}
+    }
